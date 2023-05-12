4dd2862e74c915f08e07d1e38f1656baf4a66e09
tomifuchi <tomifuchi@yahoo.com>
2023-05-12 11:34:19 +0700

    Prototype finished

--- src/js/index.js
+++ src/js/index.js
@@ -25,10 +25,13 @@
   https://en.wikipedia.org/wiki/Ultraviolet_index
 */
 
+require('../scss/style.scss');
+const {weatherJSON} = require('./modules/weather');
+
+//Fetching data related logic
+//====================================
 //Functions make url
 const makeUrl = (base, path, params) => base + '/' + path + '?' + params
-
-const appendPath = (target, path) =>  target + '/' + path; //addPath
 const appendParams = (target, params) =>  target + '&' + params; //addParams
 
 function getWeather(location) {
@@ -44,9 +47,9 @@ function getWeather(location) {
         return response.json();
       } else {
           if(response.status == 400)
-            throw Error('No location found');
+            throw new Error('No location found');
           else
-            throw Error(`Error, response with this status ${response.status}`);
+            throw new Error(`Error, response with this status ${response.status}`);
       }
     })
     .then(function(response) {
@@ -58,6 +61,7 @@ function getWeather(location) {
           temp_c: response.current.temp_c,
           feelslike_c: response.current.feelslike_c,
           condition: response.current.condition.text,
+          condition_icon: getConditionIcon(response.current.is_day, response.current.condition.code),
           wind_kph: response.current.wind_kph,
           humidity: response.current.humidity,
           cloud: response.current.cloud,
@@ -65,13 +69,62 @@ function getWeather(location) {
         }
     })
 }
-getWeather('saigon').then(render);
+getWeatherAwait('Saigon').then(render)
 
+//Await version you can wrap try catch here
+//but catch is outside so don't need
+async function getWeatherAwait(location) {
+  //Arguments to pass
+  const baseUrl = 'http://api.weatherapi.com/v1';
+  const path = 'current.json'
+  const apiKey = 'key=fdcc760077f34555b2a135105230705';
+  const query = `q=${location}`;
+
+  const url  = makeUrl(baseUrl, path, appendParams(query, apiKey));
+  let response = await fetch(url, {mode: 'cors'})
+
+  if(response.ok) {
+    response = await response.json();
+  } else {
+      if(response.status == 400)
+        throw new Error('No location found');
+      else
+        throw new Error(`Error, response with this status ${response.status}`);
+  }
+
+
+  return {
+      name: response.location.name,
+      region: response.location.region,
+      country: response.location.country,
+      localtime: response.location.localtime,
+      temp_c: response.current.temp_c,
+      feelslike_c: response.current.feelslike_c,
+      condition: response.current.condition.text,
+      condition_icon: getConditionIcon(response.current.is_day, response.current.condition.code),
+      wind_kph: response.current.wind_kph,
+      humidity: response.current.humidity,
+      cloud: response.current.cloud,
+      uv: response.current.uv,
+  }
+}
+
+//Front end related logic
+//=================================
 //cached DOM nodes for ease of access
 const cachedNodes = {
   resultContainer: document.getElementById('result-container'),
   searchBar: document.getElementById('search-bar'),
   searchbutton: document.getElementById('search-button')
+}
+
+//Based on condition code, display correct png, day or night
+function getConditionIcon(is_day, code) {
+  const obj = weatherJSON.filter((weatherObj) => weatherObj.code === code)[0];
+  if(is_day == 1) {
+    return obj.icon_day
+  } else
+    return obj.icon_night;
 }
 
 //Dom helper
@@ -84,14 +137,19 @@ function render(data) {
 
   //Loop location
   for(prop in data) {
-    tc(g('#' + prop), prop + ': ' + data[prop]);
+    if(prop !== 'condition_icon')
+      tc(g('#' + prop), data[prop]);
+    else 
+      g('#' + prop).src = data[prop];
   }
 }
 
 function renderClear() {
-  [...cachedNodes.resultContainer.childNodes].forEach((child) => {
-    tc(child, '');
+  [...cachedNodes.resultContainer.querySelectorAll('span')].forEach((child) => {
+    tc(child, 'empty');
   });
+
+  cachedNodes.resultContainer.querySelector('img').src = '';
 }
 
 //Search button
@@ -99,6 +157,11 @@ cachedNodes.searchbutton.addEventListener('click' , () => {
   if(cachedNodes.searchBar.value != '') {
     const search = cachedNodes.searchBar.value;
     cachedNodes.searchBar.value = '';
-    getWeather(search).then(render).catch((err) => {renderClear(); console.error(err);});
+    getWeather(search).then(render).catch((err) => {
+      renderClear(); 
+      console.error(err);
+      document.getElementById('search-error').textContent = 'Error: Location not found!';
+      setTimeout(() => document.getElementById('search-error').textContent = '', 3000);
+    });
   }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     plugins: [ new htmlWebpackPlugin({ title: "Weather app", filename: "index.html", template: "src/pug/index.pug", }), ], 
     module: { 
         rules: [ 
-            { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource', }, 
+            { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/inline', }, 
             { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource', }, 
             { test: /\.pug$/, use: { loader: "pug-loader", options: { pretty: true, }, }, }, 
             { test: /\.s[ac]ss$/i, use: [ "style-loader", "css-loader", "sass-loader" ], }, 
